df82ab293f81a37228988491f3151c33ccd5ab56
JayDhiman <jaydhiman01@gmail.com>
2024-01-16 10:52:47 +0530

    new Ui has been implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 
 import './App.css'
-import Form from './components/Form'
-import Imagesection from './components/Imagesection'
+
 
 function App() {
 
 
   return (
-   <>
-   <div className='container mx-auto shadow-2xl border-collapse  h-fit w-fit rounded-2xl'>
-    <div className='flex'>
- <div className='w-fit'>
-   {/* image */}
-   <Imagesection />
- </div>
-
- <div className='shadow-xl bg-neutral-900 w-fit'>
-
-   <Form/>
- </div>
-    </div>
-    
-   </div>
-   </>
+ <>
+ 
+ </>
   )
 }
 
